2ca8d7de5b444d39f6c300215e0f2e03fd1ff912
Michael Richardson <michaelrichardson@Severus.local>
2015-09-29 11:45:57 -0700

    Added lines to output result

--- count_similar_pairs.sh
+++ count_similar_pairs.sh
@@ -3,3 +3,5 @@
 python ./code/find_duplicate_sentences.py ./input/sentences.txt > ./input/dups.txt
 python ./code/count_duplicate_pairs.py ./input/dups.txt > ./results/dup_pair_count.txt
 python ./code/find_edit_distance_one.py ./input/dups.txt > ./results/sim_pair_count.txt
+cat ./results/dup_pair_count.txt
+cat ./results/sim_pair_count.txt
